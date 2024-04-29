37550e9d5d5a3f1d9f5049e47ee96e8531806a45
Thiago <itsadeadh2@gmail.com>
2024-04-29 14:24:05 -0400

    Update script

--- bin/filebrowser_batocera.sh
+++ bin/filebrowser_batocera.sh
@@ -2,7 +2,8 @@
 
 curl -fsSL https://raw.githubusercontent.com/filebrowser/get/master/get.sh | bash
 
+echo "Creating custom.sh"
 cat > /userdata/system/custom.sh <<EOF
 #!/bin/bash
-exec /usr/bin/filebrowser -r /userdata/roms -a 0.0.0.0 -p 80
+/usr/bin/filebrowser -r /userdata/roms -a 0.0.0.0 -p 80
 EOF
