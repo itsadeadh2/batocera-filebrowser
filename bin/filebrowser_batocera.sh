8199daac9682571951ccaa77cce54f28543e0267
Thiago <itsadeadh2@gmail.com>
2024-04-29 14:29:06 -0400

    Update script

--- bin/filebrowser_batocera.sh
+++ bin/filebrowser_batocera.sh
@@ -5,5 +5,25 @@ curl -fsSL https://raw.githubusercontent.com/filebrowser/get/master/get.sh | bas
 echo "Creating custom.sh"
 cat > /userdata/system/custom.sh <<EOF
 #!/bin/bash
-/usr/bin/filebrowser -r /userdata/roms -a 0.0.0.0 -p 80
+case "$1" in
+    start)
+        # Code in here will only be executed on boot.
+        echo "Starting filebrowser"
+        /usr/bin/filebrowser -r /userdata/roms -a 0.0.0.0 -p 80
+        ;;
+    stop)
+        # Code in here will only be executed on shutdown.
+        echo ""
+        ;;
+    restart|reload)
+        # Code in here will executed (when?).
+        echo ""
+        ;;
+    *)
+        # Code in here will be executed in all other conditions.
+        echo ""
+        ;;
+esac
+
+exit $?
 EOF
