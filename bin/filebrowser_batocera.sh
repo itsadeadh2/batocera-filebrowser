48a0c876889725b86bee491b6a0e1207ed4e13af
Thiago <itsadeadh2@gmail.com>
2024-04-29 15:09:53 -0400

    Update script/readme

--- bin/filebrowser_batocera.sh
+++ bin/filebrowser_batocera.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-curl -fsSL https://raw.githubusercontent.com/filebrowser/get/master/get.sh | bash
+# Download the script
+curl -fsSL https://raw.githubusercontent.com/filebrowser/get/master/get.sh -o install.sh
+
+# Modify a specific line in the script. For example, changing line 10:
+sed -i '10s/.*/install_path="/userdata/system"/' install.sh
+
+# Execute the modified script
+bash install.sh
 
 echo "Creating custom.sh"
 cat > /userdata/system/custom.sh <<'EOF'
@@ -9,7 +16,7 @@ case "$1" in
     start)
         # Code in here will only be executed on boot.
         echo "Starting filebrowser"
-        /usr/bin/filebrowser -r /userdata/roms -a 0.0.0.0 -p 80 &
+        /userdata/system/filebrowser -r /userdata/roms -a 0.0.0.0 -p 80
 ;;
 
 esac
