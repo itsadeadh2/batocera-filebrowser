88d41bfb63d3a757ff5c021a7ce3695939ba0c9b
Thiago <itsadeadh2@gmail.com>
2024-04-29 15:39:39 -0400

    Update script/readme

--- bin/filebrowser_batocera.sh
+++ bin/filebrowser_batocera.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
 
-# Download the script
+# Download the installation script from filebrowser
 curl -fsSL https://raw.githubusercontent.com/filebrowser/get/master/get.sh -o install.sh
 
-echo "Original line 29:"
-sed -n '29p' install.sh
-
+# Small modification to the script to make filebrowser be installed at /userdata/system
 sed -i '29s#.*#install_path="/userdata/system"#' install.sh
-
 
 # Execute the modified script
 bash install.sh
 
-echo "Creating custom.sh"
-cat > /userdata/system/custom.sh <<'EOF'
-#!/bin/bash
-case "$1" in
-    start)
-        # Code in here will only be executed on boot.
-        echo "Starting filebrowser"
-        /userdata/system/filebrowser -r /userdata/roms -a 0.0.0.0 -p 80
-;;
-
-esac
-
+# Check if custom.sh exists, append or create accordingly
+FILE="/userdata/system/custom.sh"
+if [ -f "$FILE" ]; then
+    echo "Appending to existing custom.sh"
+    cat >> "$FILE" <<'EOF'
+/userdata/system/filebrowser -r /userdata/roms -a 0.0.0.0 -p 80
 EOF
+else
+    echo "Creating custom.sh"
+    cat > "$FILE" <<'EOF'
+#!/bin/bash
+/userdata/system/filebrowser -r /userdata/roms -a 0.0.0.0 -p 80
+EOF
+fi
